5a0716a01fdad6b9f78679410605c63cff4c08e7
xowjd1 <taejeong311@gmail.com>
2025-08-27 08:21:21 +0900

    before sound

--- Assets/KTJ/Scripts/TreeMissionClear.cs
+++ Assets/KTJ/Scripts/TreeMissionClear.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using System.Collections;
 
@@ -9,58 +8,72 @@ public class TreeMissionClear : MonoBehaviour
     public Rigidbody handAxe;
     public BoxCollider handAxeCol;
     public PlayerChopping playerChopping;
-    
+
     public float destroyDelay = 3f;
 
-    public bool treesCleared= false;
+    public bool treesCleared = false;
 
-    private bool waitStarted = false;
+    bool waitStarted = false;
+    bool finalized = false;
 
-    private void Awake()
+    void Awake()
     {
-        handAxeCol.enabled = false;
-        handAxe.isKinematic = true;
-        handAxe.useGravity = false;
+        if (handAxeCol) handAxeCol.enabled = false;
+        if (handAxe)
+        {
+            handAxe.isKinematic = true;
+            handAxe.useGravity  = false;
+        }
     }
 
     void Update()
     {
-        if (waitStarted) return;
-
-        if (IsDropped(treePart1) && IsDropped(treePart2))
-        {
+        if (!waitStarted && IsDropped(treePart1) && IsDropped(treePart2))
             StartCoroutine(ClearAfterDelay());
-        }
-
-        if (treesCleared)
-        {
-            playerChopping.enabled = false;
-            handAxe.gameObject.transform.SetParent(null);
-            handAxe.isKinematic = false;
-            handAxe.useGravity = true;
-            handAxeCol.enabled = true;
-        }
     }
 
     bool IsDropped(Rigidbody rb)
     {
-        if (!rb) return false;
-        return rb.useGravity && !rb.isKinematic;
+        return rb && rb.useGravity && !rb.isKinematic;
     }
 
     IEnumerator ClearAfterDelay()
     {
         waitStarted = true;
+
         yield return new WaitForSeconds(destroyDelay);
 
-        if (treePart1)
-            treePart1.gameObject.SetActive(false);
-        if (treePart2)
-            treePart2.gameObject.SetActive(false);
-
+        if (treePart1) treePart1.gameObject.SetActive(false);
+        if (treePart2) treePart2.gameObject.SetActive(false);
 
         treesCleared = true;
 
+        // ▼ 여기서 바로 마무리 처리
+        FinalizeMission();
+        // 이 스크립트 더이상 필요 없으면 비활성화
+        // enabled = false;
     }
-    
+
+    void FinalizeMission()
+    {
+        if (finalized) return;
+        finalized = true;
+
+        // 플레이어 채핑 종료 및 비활성
+        if (playerChopping)
+        {
+            playerChopping.ChopEnd();   // 이동락/비주얼 복구
+            playerChopping.enabled = false;
+        }
+
+        // 손도끼 드롭
+        if (handAxe)
+        {
+            handAxe.transform.SetParent(null, true);
+            handAxe.isKinematic = false;
+            handAxe.useGravity  = true;
+            handAxe.WakeUp();
+        }
+        if (handAxeCol) handAxeCol.enabled = true;
+    }
 }
