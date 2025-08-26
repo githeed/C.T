96a5f15987b33ff274bd1a97260894f45d8cbea3
xowjd1 <taejeong311@gmail.com>
2025-08-27 03:27:27 +0900

    dig shovel transform fix / shovel detach

--- Assets/HSH/00.Script/Triggers/ShovelFinTrigger.cs
+++ Assets/HSH/00.Script/Triggers/ShovelFinTrigger.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class ShovelFinTrigger : MonoBehaviour
 {
     bool isTriggerd = false;
+    public Rigidbody handShovelrb;
+    public BoxCollider handShovelBoxCollider;
+    public TerrainDigger terrainDigger;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,6 +16,12 @@ public class ShovelFinTrigger : MonoBehaviour
             GameManager.Instance.status = GameStatus.TapeMission;
             GameManager.Instance.SetCompleteUI();
             GameManager.Instance.OnMissionComplete();
+
+            terrainDigger.hasShovel = false;
+            handShovelBoxCollider.enabled = true;
+            handShovelrb.isKinematic = false;
+            handShovelrb.useGravity = true;
+            handShovelrb.gameObject.transform.SetParent(null);
         }
     }
 }
